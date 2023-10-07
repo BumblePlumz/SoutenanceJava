ad102e37c26206df62417ce6ad16b73f79c897d5
BumblePLumz <bumbleplumz@gmail.com>
2023-10-07 02:05:17 +0200

    07-10-2023 v0.05

---
+++ src/main/java/util/Calcul.java
@@ -0,0 +1,20 @@
+package util;
+
+public class Calucl {
+    /**
+     * Soustraction de deux nombres
+     * @param n1 premier nombre
+     * @param n2 deuxième nombre
+     * @param bool true renvoie la valeur absolue de la soustraction
+     * @return le resultat de la soustraction
+     */
+    public static int soustraction (int n1, int n2, boolean bool){
+        if (bool){
+            return Math.abs(n1 - n2);
+        }else{
+            return n1-n2;
+        }
+    }
+
+
+}
